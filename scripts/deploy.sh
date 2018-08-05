cbcc9888d984ae2aebc21d24085d66e8ce4ca7ce
Cjmash <jamescollins.jcparty@gmail.com>
2018-08-05 10:57:48 +0300

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -21,11 +21,7 @@ require 'DOCKER_REGISTRY' $DOCKER_REGISTRY
 require 'GCLOUD_SERVICE_KEY' $GCLOUD_SERVICE_KEY
 getHosts(){ 
     echo "============> geting hosts "
-    if [ "$CIRCLE_BRANCH" == 'master' ]; then
-      CURRENTIPS="$(gcloud compute instances list --project bench-projects | grep  gke-bench-production-default-pool | awk -v ORS=, '{if ($4) print $4}' | sed 's/,$//')"
-    else
-      CURRENTIPS="$(gcloud compute instances list --project bench-projects | grep  gke-bench-staging-default-pool | awk -v ORS=, '{if ($4) print $4}' | sed 's/,$//')"
-    fi
+      CURRENTIPS="$(gcloud compute instances list --project bench-projects | grep  gke-bench- | awk -v ORS=, '{if ($4) print $4}' | sed 's/,$//')"
 }
 
 buildAndTagDockerImages() {
