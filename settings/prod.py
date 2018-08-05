508c79821c061e9935efae9bd81b46fd89d6e452
Cjmash <jamescollins.jcparty@gmail.com>
2018-08-05 14:43:53 +0300

    Update prod.py

--- settings/prod.py
+++ settings/prod.py
@@ -2,7 +2,7 @@ from .base import *  # noqa: F403,F401
 import os
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get(HOST_IP),
+ALLOWED_HOSTS = [os.environ.get('HOST_IP'),
     'art-api.andela.com'
 ]
 
